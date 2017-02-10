cfae794478c9638ecc6793f81ca69cedf2ed2a4a
Rick Breidenstein <RickB17@users.noreply.github.com>
2017-02-10 11:27:47 -0500

    Update lockitron.py

--- config/custom_components/sensor/lockitron.py
+++ config/custom_components/sensor/lockitron.py
@@ -1,28 +1,14 @@
-"""
-Lockitron Sensor
+"""Lockitron Sensor.
 by Rick Breidenstein
 www.virtualrick.com
-
-sample configuration.yaml entries
-sensor:
-  - platform: lockitron
-    lock_name: 'VirtualLock'
-    lock_uuid: 'YOURUUIDFORYOURLOCK'
-    access_token: 'YOURACCESSTOKENHERE'
-  - platform: lockitron
-    lock_name: 'Some Other Door'
-    lock_uuid: 'YOURUUIDFORYOURLOCK'
-    access_token: 'YOURVERYLONGACCESSTOKENHERE'
 """
+
 import logging
 import voluptuous as vol
 import requests
 
 from homeassistant.helpers.entity import Entity
 from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.const import (
-    CONF_NAME, CONF_VALUE_TEMPLATE, STATE_UNKNOWN)
-from homeassistant.helpers.entity import Entity
 import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
@@ -46,7 +32,10 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     access_token = config.get(CONF_ACCESS_TOKEN)
     lock_name = config.get(CONF_LOCK_NAME)
 
-    add_devices([LockitronSensor(lock_name, 'Unlocked', lock_uuid, access_token)])
+    add_devices(
+        [LockitronSensor(lock_name, 'INIT', lock_uuid, access_token)]
+        )
+
 
 class LockitronSensor(Entity):
     """Representation of a Lockitron sensor."""
@@ -60,6 +49,7 @@ class LockitronSensor(Entity):
 
     @property
     def should_poll(self):
+        """Enable polling."""
         return True
 
     @property
@@ -70,47 +60,40 @@ class LockitronSensor(Entity):
     @property
     def state(self):
         """Return the state of the sensor."""
-        try:
-            url = "https://api.lockitron.com/v2/locks/" + self._lock_uuid + "?access_token=" + self._access_token
-            r = requests.get(url)
-            resp = r.json()
-            self._state = resp["state"]
-        except:
-            self._state = 'FAILED'
+        url = (
+            "https://api.lockitron.com/v2/locks/" + self._lock_uuid +
+            "?access_token=" + self._access_token
+            )
+        req = requests.get(url)
+        resp = req.json()
+        self._state = resp["state"]
         return self._state
-
-    def update(self):
-        _LOGGER.info('Updating Lockitron state')
-        """Return the state of the sensor."""
-        try:
-            url = "https://api.lockitron.com/v2/locks/" + self._lock_uuid + "?access_token=" + self._access_token
-            r = requests.get(url)
-            resp = r.json()
-            self._state = resp["state"]
-        except:
-            self._state = 'FAILED'
-        return self._state
-
-class LockitronSensorData(object):
-    """The class for handling the data retrieval."""
-
-    def __init__(self, command):
-        """Initialize the data object."""
-        lock_uuid = config.get(CONF_LOCK_UUID)
-        access_token = config.get(CONF_ACCESS_TOKEN)
-        lock_name = config.get(CONF_LOCK_NAME)
 
     def update(self):
         """Get the latest data with a shell command."""
         _LOGGER.info('Updating Lockitron state for ' + self._lock_name)
-        _LOGGER.info(self._lock_uuid)
-        """Return the state of the sensor."""
-        self._state = ''
-        try:
-            url = "https://api.lockitron.com/v2/locks/" + self._lock_uuid + "?access_token=" + self._access_token
-            r = requests.get(url)
-            resp = r.json()
-            self._state = resp["state"]
-        except:
-            self._state = 'FAILED'
+        url = (
+            "https://api.lockitron.com/v2/locks/" + self._lock_uuid +
+            "?access_token=" + self._access_token
+            )
+        req = requests.get(url)
+        resp = req.json()
+        self._state = resp["state"]
+        return self._state
+
+
+class LockitronSensorData(LockitronSensor):
+    """The class for handling the data retrieval."""
+
+    def update(self):
+        """Get the latest data with a shell command."""
+        _LOGGER.info('Updating Lockitron state for ' + self._lock_name)
+        url = (
+            "https://api.lockitron.com/v2/locks/" + self._lock_uuid +
+            "?access_token=" + self._access_token
+            )
+        req = requests.get(url)
+        resp = req.json()
+        self._state = resp["state"]
+        _LOGGER.info('Insdie Try ' + self._state)
         return self._state
